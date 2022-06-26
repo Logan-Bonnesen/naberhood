22464a0622bfa0492699111c436a4d81ff0238ed
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2022-06-26 14:54:54 -0700

    changing import for thumbsUp, delete, and moreHoriz icons but leaving commented out

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography, Icon } from '@material-ui/core';
-// import ThumbUpAltIcon from '@material-ui/core';
-import DeleteIcon from '@material-ui/core';
-// import Icon from '@material-ui/core';
+// import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+// import DeleteIcon from '@material-ui/icons/Delete';
+// import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
 
