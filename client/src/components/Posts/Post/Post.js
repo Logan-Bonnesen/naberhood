98dca18cee6817a51e87461ca345bdab68f6031b
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2022-06-27 21:37:34 -0700

    adding thumbUp, delete, and morehoriz icons

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography, Icon } from '@material-ui/core';
-// import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
-// import DeleteIcon from '@material-ui/icons/Delete';
-// import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
 
@@ -21,8 +21,7 @@ const Post = ({ post, setCurrentId}) => {
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
             <div className={classes.overlay2}>
-                {/* fix icon/morehorizicon issue */}
-                <Button style={{ color: 'white' }} size="small" onClick={() => setCurrentId(post._id)}><Icon fontSize='default'/></Button>
+                <Button style={{ color: 'white' }} size="small" onClick={() => setCurrentId(post._id)}><MoreHorizIcon fontSize="medium"/></Button>
             </div>
             <div className={classes.details}>
                 <Typography variant="body2" color="textSecondary" component="h2" >{post.tags.map((tag) => `#${tag}`)}</Typography>
@@ -32,10 +31,8 @@ const Post = ({ post, setCurrentId}) => {
                 <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                {/* add <ThumbUpAltIcon fontSize="small" /> functionality */}
-                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}> Like {post.likeCount} </Button>
-                {/* add <DeleteIcon fontSize="small" /> functionality */}
-                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}> Delete</Button>
+                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" /> Like {post.likeCount} </Button>
+                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}><DeleteIcon fontSize="small" /> Delete</Button>
             </CardActions>
         </Card>
     );
