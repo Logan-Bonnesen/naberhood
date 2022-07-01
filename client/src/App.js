aa055d1395347f163cb8313d04935453a8de72dd
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2022-06-30 17:09:38 -0700

    updating mobile responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 import memories from './images/memories.png';
 import useStyles from './styles'
+// import sergey from './images/naberhood_sergey.jpg'
 
 const App = () => {
     const [currentId, setCurrentId] = useState(0);
@@ -20,12 +21,12 @@ const App = () => {
     return (
         <Container maxWidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Naberhood</Typography>
+                <Typography className={classes.heading} variant="h2" align="center">The Naberhood</Typography>
                 <img className={classes.image} src={memories} alt="memories" height="60"/>
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                    <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
                             <Posts setCurrentId={setCurrentId} />
                         </Grid>
